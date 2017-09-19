b9f74477dac4571515facd0b8aad68b04cf68ab2
Yauhen_Tumashchyk <Yauhen.Tumashchyk@ihg.com>
2017-09-19 16:14:14 +0300

    add data provider for test

--- src/test/java/TestingWebDriver.java
+++ src/test/java/TestingWebDriver.java
@@ -1,9 +1,9 @@
-
 import static org.testng.Assert.fail;
 import static util.VerifyHelper.linksContainsText;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import driver.DriverSingleton;
@@ -14,7 +14,6 @@ public class TestingWebDriver
 {
     private GoogleByMainPage googleByMainPage;
     private ResultPage resultPage;
-    private String textForCheck = "beatles";
 
     @BeforeMethod
     public void initialize()
@@ -23,20 +22,27 @@ public class TestingWebDriver
         resultPage = new ResultPage(DriverSingleton.getDriver());
     }
 
-    @Test
-    public void isTextPresentedInAllLinksTest()
+    @DataProvider(name = "text for check")
+    public Object[][] getText() throws Exception {
+        return new Object[][]{
+                {"beatles"},
+                {"selenium"},
+        };
+    }
+
+    @Test(dataProvider = "text for check")
+    public void isTextPresentedInAllLinksTest(String text)
     {
         googleByMainPage.openPage();
-        googleByMainPage.inputText(textForCheck);
+        googleByMainPage.inputText(text);
         googleByMainPage.submitGoogleByPage();
 
-        if (!linksContainsText(resultPage.getAllLinksTxt(), textForCheck.toLowerCase()))
+        if (!linksContainsText(resultPage.getAllLinksTxt(), text.toLowerCase()))
         {
             fail("Not all elements contain the search text");
         } else {
             System.out.println("All elements contain the search text");
         }
-
     }
 
     @AfterMethod
